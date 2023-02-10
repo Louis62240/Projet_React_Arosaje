5e395081bc4bbee37d915c3c55ceb838cd1d1bc1
machadoquentin <qmach.dev@gmail.com>
2023-02-10 14:04:13 +0100

    Ajout Connexion

---
+++ src/components/Connexion.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const Connexion = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Nom d'utilisateur:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Mot de passe :</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default LoginForm;
