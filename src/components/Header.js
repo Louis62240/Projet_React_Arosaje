0e0109270a2ccc4500731bc7c6b010821517eff0
louis <l.hanquiez22@gmail.com>
2023-02-10 14:01:29 +0100

    ++

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,31 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Container } from "reactstra
 
 const Header = () => {
   return (
-    <Navbar color="light" light expand="md">
-      <Container>
-        <NavbarBrand href="/">Nom de votre entreprise</NavbarBrand>
-        <Nav className="ml-auto" navbar>
-          <NavItem>
-            <NavLink href="#">Lien 1</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Lien 2</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#">Lien 3</NavLink>
-          </NavItem>
-        </Nav>
-        <img src="path/to/your/logo.png" alt="Logo" style={{ height: "50px", marginLeft: "10px" }} />
-      </Container>
-    </Navbar>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+        crossorigin="anonymous"
+      />
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand href="/">Nom de votre entreprise</NavbarBrand>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Lien 1</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Lien 2</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Lien 3</NavLink>
+            </NavItem>
+          </Nav>
+          <img src="path/to/your/logo.png" alt="Logo" style={{ height: "50px", marginLeft: "10px" }} />
+        </Container>
+      </Navbar>
+    </>
   );
 };
 
