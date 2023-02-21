f6654c3d5072165a4d0bf1f0dbea95453cbfc3f7
louis <l.hanquiez22@gmail.com>
2023-02-21 09:36:54 +0100

    ++

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,12 @@ const Header = () => {
   const [showAccueil, setShowAccueil] = useState(true);
   const [showVosPlantes, setShowVosPlantes] = useState(false);
 
+  useState(() => {
+    setShowAccueil(true);
+    setShowVosPlantes(false);
+  }, []);
+
+  if (showAccueil) {
   return (
     <>
       <link
@@ -78,8 +84,96 @@ const Header = () => {
           </NavbarText>
         </Container>
       </Navbar>
+      <CSSTransition
+        in={showAccueil}
+        timeout={300}
+        classNames="accueil"
+        unmountOnExit
+      >
+        <Accueil />
+      </CSSTransition>
     </>
+  
   );
+            } else if (showVosPlantes) {
+              return (
+                <>
+                  <link
+                    rel="stylesheet"
+                    href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+                    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+                    crossorigin="anonymous"
+                  />
+                  <link
+                    rel="stylesheet"
+                    href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+                    integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc"
+                    crossorigin="anonymous"
+                  />
+                  <Navbar
+                    style={{ backgroundColor: "rgb(157 236 190)" }}
+                    light
+                    expand="md"
+                  >
+                    <Container className="d-flex align-items-center m-0 ">
+                      <img
+                        src={require("../assets/img/logo.png")}
+                        alt="Logo"
+                        className="logo"
+                        height={100}
+                      />
+                      <Nav className="d-flex align-items-center">
+                        <NavItem>
+                          <NavLink
+                            className="NavMenuLink"
+                            onClick={() => {
+                              setShowAccueil(true);
+                              setShowVosPlantes(false);
+                            }}
+                          >
+                            Accueil
+                          </NavLink>
+                        </NavItem>
+                        <NavItem>
+                          <NavLink
+                            className="NavMenuLink"
+                            onClick={() => {
+                              setShowAccueil(false);
+                              setShowVosPlantes(true);
+                            }}
+                          >
+                            Vos Plantes
+                          </NavLink>
+                        </NavItem>
+                      </Nav>
+                      <NavbarText>
+                        <i
+                          className="fas fa-user fa-2x"
+                          style={{ cursor: "pointer" }}
+                          onMouseEnter={(e) => {
+                            e.target.style.transform = "scale(1.2)";
+                            e.target.style.transition = "0.5s";
+                          }}
+                          onMouseLeave={(e) => {
+                            e.target.style.transform = "scale(1)";
+                            e.target.style.transform = "0.5s";
+                          }}
+                        ></i>
+                      </NavbarText>
+                    </Container>
+                  </Navbar>
+                  <CSSTransition
+                    in={showVosPlantes}
+                    timeout={300}
+                    classNames="vosPlantes"
+                    unmountOnExit
+                  >
+                    <VosPlantes />
+                  </CSSTransition>
+                </>
+              );
+            }
+            
 };
 
 export default Header;
