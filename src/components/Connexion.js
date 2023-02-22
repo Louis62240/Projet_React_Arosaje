99f635178aa90c54d32bc30603df85115d58563c
machadoquentin <qmach.dev@gmail.com>
2023-02-22 12:03:43 +0100

    Fin de formualire connexion

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -40,14 +40,14 @@ const Connexion = ({ onConnect }) => {
           <div className="form">
             {showRegisterForm ? (
               <form className="register-form">
-                <input type="text" placeholder="name" />
-                <input type="password" placeholder="mot de passe" />
-                <input type="text" placeholder="email" />
-                <button>create</button>
+                <input type="text" placeholder="Nom" />
+                <input type="password" placeholder="Mot de passe" />
+                <input type="text" placeholder="Adresse mail" />
+                <button>Crée un compte</button>
                 <p className="message">
-                  Already registered?{" "}
+                  Déjà inscrit ?{" "}
                   <a href="#" onClick={handleHideRegisterForm}>
-                    Sign In
+                    Se connecter
                   </a>
                 </p>
               </form>
