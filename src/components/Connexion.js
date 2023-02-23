c591b1c48054e9d7a828fb38ca5f1cd8b00690b8
louis <l.hanquiez22@gmail.com>
2023-02-23 11:25:23 +0100

    ++ form connexion

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -6,6 +6,7 @@ function Connexion({ onConnect }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
+  const [isLoginForm, setIsLoginForm] = useState(true); // state pour savoir quel formulaire afficher
 
   const handleEmailChange = (event) => setEmail(event.target.value);
   const handlePasswordChange = (event) => setPassword(event.target.value);
@@ -22,29 +23,54 @@ function Connexion({ onConnect }) {
     }
   };
 
+  const handleSwitchForm = () => {
+    setIsLoginForm(!isLoginForm); // Inverser l'état du formulaire affiché
+  };
+
   return (
     <div className='PlacementCard'>
-    <div className="card">
-    <img
-            src={require("../assets/img/logo.png")}
-            alt="Logo"
-            className="logo"
-            height={100}
-          />
-      <form className='formConnexion' onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label className='labelConnexion' htmlFor="email">Adresse email :</label>
-          <input className='InputConnexion' type="email" id="email" name="email" value={email} onChange={handleEmailChange} />
-        </div>
-        <div className="form-group">
-          <label className='labelConnexion' htmlFor="password">Mot de passe :</label>
-          <input className='InputConnexion' type="password" id="password" name="password" value={password} onChange={handlePasswordChange} />
-        </div>
-        {errorMessage && <div className="error">{errorMessage}</div>}
-        <button className="btn" type="submit">Se connecter</button>
-        
-      </form>
-    </div>
+      <div className="card">
+        <img
+          src={require("../assets/img/logo.png")}
+          alt="Logo"
+          className="logo"
+          height={100}
+        />
+        {isLoginForm ? (
+          <form className='formConnexion' onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label className='labelConnexion' htmlFor="email">Adresse email :</label>
+              <input className='InputConnexion' type="email" id="email" name="email" value={email} onChange={handleEmailChange} />
+            </div>
+            <div className="form-group">
+              <label className='labelConnexion' htmlFor="password">Mot de passe :</label>
+              <input className='InputConnexion' type="password" id="password" name="password" value={password} onChange={handlePasswordChange} />
+            </div>
+            {errorMessage && <div className="error">{errorMessage}</div>}
+            <br/>
+            <button className="btn" type="submit">Se connecter</button><br/>
+            <a className='buttonSecondaire' onClick={handleSwitchForm}>Vous ne possèdez pas de Compte ? Cliquez ici</a>
+          </form>
+        ) : (
+          <form className='formInscription'>
+            <div className="form-group">
+              <label className='labelConnexion' htmlFor="email">Adresse email :</label>
+              <input className='InputConnexion' type="email" id="email" name="email" />
+            </div>
+            <div className="form-group">
+              <label className='labelConnexion' htmlFor="password">Mot de passe :</label>
+              <input className='InputConnexion' type="password" id="password" name="password" />
+            </div>
+            <div className="form-group">
+              <label className='labelConnexion' htmlFor="confirmPassword">Confirmer le mot de passe :</label>
+              <input className='InputConnexion' type="password" id="confirmPassword" name="confirmPassword" />
+            </div>
+            <br/>
+            <button className="btn" type="submit">S'inscrire</button><br/>
+            <a className='buttonSecondaire' onClick={handleSwitchForm}>Vous avez déja un compte ? Cliquez ici</a>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
