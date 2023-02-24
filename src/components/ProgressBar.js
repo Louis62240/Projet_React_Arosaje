0160f2c4548a82dda6d75e0d8f9aa3c74ab2075e
louis <l.hanquiez22@gmail.com>
2023-02-24 15:05:55 +0100

    ++ ajout utilisateur

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../assets/css/ProgressBar.css";
+
+const ProgressBar = ({ currentStep }) => {
+    console.log(currentStep)
+  return (
+    <div className="progress-bar-container">
+      <div className={`progress-bar-step ${currentStep >= 1 ? "completed" : ""}`}>
+        <div className="progress-bar-circle">1</div>
+        <div className="progress-bar-label">Etape 1</div>
+      </div>
+      <div className={`progress-bar-step ${currentStep >= 2 ? "completed" : ""}`}>
+        <div className="progress-bar-circle">2</div>
+        <div className="progress-bar-label">Etape 2</div>
+      </div>
+      <div className={`progress-bar-step ${currentStep >= 3 ? "completed" : ""}`}>
+        <div className="progress-bar-circle">3</div>
+        <div className="progress-bar-label">Etape 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
